4b48c37d00fbf8ebea7f7aae80ade1aba9444605
K0R0B0R0S <joseasfer07@gmail.com>
2025-04-27 02:07:09 -0300

    refactor: movendo imagens e rotulos

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     model.train(
         data='./data/metadata/data.yaml',
-        epochs=10,
+        epochs=50,
         imgsz=640,
         device=0,
         project='./models', 
